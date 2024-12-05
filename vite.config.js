9e6090578d4b8846568233667e24cf6dfbd8c4b6
ritaban06 <ghoshritaban2006@gmail.com>
2024-12-05 20:11:39 +0530

    Removed server part

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Define rewrites here for development purposes
-    // middlewareMode: 'html',
-    proxy: {
-      '/': {
-        target: '/index.html',
-        rewrite: (path) => path.replace(/^(\/.*)/, '/index.html'),
-      },
-    },
-  },
+  // server: {
+  //   // Define rewrites here for development purposes
+  //   // middlewareMode: 'html',
+  //   proxy: {
+  //     '/': {
+  //       target: '/index.html',
+  //       rewrite: (path) => path.replace(/^(\/.*)/, '/index.html'),
+  //     },
+  //   },
+  // },
 });
