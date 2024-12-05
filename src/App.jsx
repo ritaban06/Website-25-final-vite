05b73ea4750fdd00c3de1cef6fb9056cf00b4228
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2024-12-05 18:12:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,39 +10,6 @@ import Footer from './components/footer/Footer';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    console.log(error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
-    }
-    return this.props.children;
-  }
-}
-
-function App() {
-
-    useEffect(() => {
-        console.log('App mounted');
-        AOS.init({
-          duration : 2000
-        }); 
-      }, []);
-
-    console.log('App rendering');
-
     return (
         <>
             
